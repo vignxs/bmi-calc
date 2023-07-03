f981541d32728e34ee4e59a923c96459cc7a0f70
vignxs <vignxs@gmail.com>
2023-07-03 16:45:40 +0530

    added different shadow

--- bmi-frontend/src/components/BMIHome.tsx
+++ bmi-frontend/src/components/BMIHome.tsx
@@ -290,11 +290,13 @@ function BMIHome() {
           </Box>
         </Paper>
         <Paper
-          elevation={3}
           sx={{
             marginTop: "20px",
             padding: "10px",
             height: "380px",
+            transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+            boxShadow:
+              "rgba(145, 158, 171, 0.2) 0px 0px 2px 0px, rgba(145, 158, 171, 0.12) 0px 12px 24px -4px",
             width: "230px",
           }}
         >
