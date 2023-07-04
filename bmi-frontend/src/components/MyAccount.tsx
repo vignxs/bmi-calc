102a4f7e62c4b1d50d140d2364933f5ea0c23742
vignxs <vignxs@gmail.com>
2023-07-04 12:39:32 +0530

    updated code

--- bmi-frontend/src/components/MyAccount.tsx
+++ bmi-frontend/src/components/MyAccount.tsx
@@ -1,34 +1,64 @@
-import { Typography, Container, TextField, Box, FormControl, Button } from "@mui/material";
+import { Typography, Container, TextField, Box } from "@mui/material";
 import { getUserInfoFromSession } from "../utils/SessionUtils";
-import { TFStyle } from "../utils/Constants";
+import { TFStyle, columns } from "../utils/Constants";
 import ResponsiveAppBar from "../utils/ResponsiveAppBar";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
 
 const MyAccountPage = () => {
   // Retrieve user information from session
   const userInfo = getUserInfoFromSession();
   const  mobile  = 1111111;
   const bmiHistory = [
-    { id:1, name: 'akash', mobile: "111111111", age: 30, height: 170, weight: 70, bmi: 24.2 },
-    { id:2, name: 'vignesh', mobile: "222222222", age: 35, height: 165, weight: 75, bmi: 27.5 },
-    { id:3, name: 'dinesh', mobile: "333333333", age: 28, height: 180, weight: 85, bmi: 26.2 },
+    {
+      id: 1,
+      name: "akash",
+      mobile: "111111111",
+      age: 30,
+      height: 170,
+      weight: 70,
+      bmi: 24.2,
+      result: "normal",
+    },
+    {
+      id: 2,
+      name: "vignesh",
+      mobile: "222222222",
+      age: 35,
+      height: 165,
+      weight: 75,
+      bmi: 27.5,
+      result: "obese",
+    },
+    {
+      id: 3,
+      name: "dinesh",
+      mobile: "333333333",
+      age: 28,
+      height: 180,
+      weight: 85,
+      bmi: 26.2,
+      result: "underweight",
+    },
+    {
+      id: 4,
+      name: "dhanush",
+      mobile: "333333333",
+      age: 28,
+      height: 180,
+      weight: 85,
+      bmi: 26.2,
+      result: "overweight",
+    },
     // Add more sample data as needed
   ];
 
-  const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 20 },
-    { field: "name", headerName: "Name", width: 70 },
-    { field: "mobile", headerName: "Mobile", width: 100 },
-    { field: "age", headerName: "Age", width: 30},
-    { field: "height", headerName: "Height", width: 70 },
-    { field: "weight", headerName: "Weight", width: 70 },
-    { field: "bmi", headerName: "BMI", width: 80 },
-  ];
+
+
+ 
 
   return (
     <ResponsiveAppBar>
-      <Container maxWidth="sm" sx={TFStyle}>
+      <Container maxWidth="md" sx={TFStyle}>
         <Typography
           variant="h5"
           noWrap
@@ -44,51 +74,52 @@ const MyAccountPage = () => {
         >
           Account
         </Typography>
-        <div style={{width:"500px"}}>
-        <TextField
-          label="Name"
-          name="name"
-          value={userInfo?.name}
-          fullWidth
-          required
-        />
-        <TextField
-          label="Mobile"
-          name="mobile"
-          value={mobile}
-          fullWidth
-          required
-        />
-        <Typography
-          variant="h5"
-          noWrap
-          component="a"
-          href=""
-          sx={{
-            fontFamily: "Geologica",
-            fontWeight: 500,
-            color: "#000000",
-            textDecoration: "none",
-          }}
-        >
-          History
-        </Typography>
-        <Box sx={{ height: 400,mt:2, width: "100%" }}>
+        <div style={{ width: "500px" }}>
+          <TextField
+            label="Name"
+            name="name"
+            value={userInfo?.name}
+            fullWidth
+            required
+          />
+          <TextField
+            label="Mobile"
+            name="mobile"
+            value={mobile}
+            fullWidth
+            required
+          />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              fontFamily: "Geologica",
+              fontWeight: 500,
+              color: "#000000",
+              textDecoration: "none",
+            }}
+          >
+            History
+          </Typography>
+        </div>
+
+        <Box sx={{ height: 400, mt: 2, width: "90%" }}>
           <DataGrid
             rows={bmiHistory}
             columns={columns}
             initialState={{
-                pagination:{
-                    paginationModel: {
-                        pageSize:5
-                    }
-                }
+              pagination: {
+                paginationModel: {
+                  pageSize: 5,
+                },
+              },
             }}
             pageSizeOptions={[5]}
             disableRowSelectionOnClick
           />
         </Box>
-        </div>
       </Container>
     </ResponsiveAppBar>
   );
--- bmi-frontend/src/utils/Constants.tsx
+++ bmi-frontend/src/utils/Constants.tsx
@@ -1,3 +1,5 @@
+import { GridColDef } from "@mui/x-data-grid";
+
 export const TFStyle = {
   transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
   boxShadow:
@@ -46,3 +48,110 @@ export interface BMIHistory {
   bmi: number;
 }
 
+const getResultTextColor = (result: string) => {
+  switch (result) {
+    case "normal":
+      return "rgb(17, 141, 87)"; // Green
+    case "overweight":
+      return "rgb(183, 110, 0)"; // Orange
+    case "underweight":
+      return "rgb(13, 71, 161)"; // Blue
+    case "obese":
+      return "rgb(183, 29, 24)"; // Red
+    default:
+      return "";
+  }
+};
+
+const getResultBackgroundColor = (result: string) => {
+  switch (result) {
+    case "normal":
+      return "rgba(34, 197, 94, 0.16)"; // Green
+    case "overweight":
+      return "rgba(255, 171, 0, 0.16)"; // Orange
+    case "underweight":
+      return "rgba(13, 71, 161, 0.16)"; // Blue
+    case "obese":
+      return "rgba(255, 86, 48, 0.16)"; // Red
+    default:
+      return "";
+  }
+};
+
+export const columns: GridColDef[] = [
+  {
+    field: "id",
+    align: "center",
+    headerAlign: "center",
+    headerName: "ID",
+    width: 50,
+  },
+  {
+    field: "name",
+    align: "center",
+    headerAlign: "center",
+    headerName: "Name",
+    width: 150,
+  },
+  {
+    field: "mobile",
+    align: "center",
+    headerAlign: "center",
+    headerName: "Mobile",
+    width: 100,
+  },
+  {
+    field: "age",
+    align: "center",
+    headerAlign: "center",
+    headerName: "Age",
+    width: 50,
+  },
+  {
+    field: "height",
+    align: "center",
+    headerAlign: "center",
+    headerName: "Height",
+    width: 90,
+  },
+  {
+    field: "weight",
+    align: "center",
+    headerAlign: "center",
+    headerName: "Weight",
+    width: 90,
+  },
+  {
+    field: "bmi",
+    align: "center",
+    headerAlign: "center",
+    headerName: "BMI",
+    width: 80,
+  },
+  {
+    field: "result",
+    align: "center",
+    renderCell: (params) => (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: getResultTextColor(params.value as string),
+          backgroundColor: getResultBackgroundColor(params.value as string),
+          borderRadius: 6,
+          padding: "8px",
+          fontFamily:"Poppins",
+          textTransform:"capitalize", 
+          fontSize: "0.75rem",
+          fontWeight: 700,  
+        }}
+      >
+        {params.value}
+      </div>
+    ),
+    headerAlign: "center",
+    headerName: "Result",
+    width: 150,
+  },
+];
